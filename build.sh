f19ffc7033fcbc3a621da8530076beede38645bb
CodecMannProject <nike.style@outlook.com>
2025-08-05 17:00:09 +0300

    build change3

--- build.sh
+++ build.sh
@@ -8,4 +8,4 @@ python manage.py makemigrations
 
 python manage.py migrate
 
-python manage.py createsuperuser --no-input
+python manage.py createsuperuser --no-input --username $DJANGO_SUPERUSER_USERNAME --email $DJANGO_SUPERUSER_EMAIL || true
