b61d901ea68ea5955922e279d36d9a7321cc0b7b
CodecMannProject <nike.style@outlook.com>
2025-08-06 13:27:28 +0300

    fixed for me

--- encryptserver/settings.py
+++ encryptserver/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3fyrk($s60xlf1#-gmtiw_h$5yjpn2^)hi2+=jo7!3$^+55ypa'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
