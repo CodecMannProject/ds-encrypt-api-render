64d08864a2cb567386f6466f9998771e6c4865ad
CodecMannProject <nike.style@outlook.com>
2025-08-06 13:47:54 +0300

    cors for discord

--- encryptserver/settings.py
+++ encryptserver/settings.py
@@ -41,10 +41,12 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework.authtoken',
-    'keyapi'
+    'corsheaders',
+    'keyapi',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -146,3 +148,18 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
     ]
 }
+
+# CORS
+CORS_ALLOWED_ORIGINS = [
+    "https://discord.com"
+]
+
+CORS_ALLOW_METHODS = [
+    "GET",
+    "POST",
+]
+
+CORS_ALLOW_HEADERS = [
+    "authorization",
+    "content-type",
+]
